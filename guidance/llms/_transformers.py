89d1ad142b2d431148b02087702147467228ac84
Aleksey Korshuk <ale-kor02@mail.ru>
2023-05-28 01:13:28 -0700

    updates

--- guidance/llms/_transformers.py
+++ guidance/llms/_transformers.py
@@ -295,8 +295,8 @@ class TransformersSession(LLMSession):
             print("len", len(input_ids[0]))
 
             if max_tokens + len(input_ids[0]) > max_context:
-
                 input_ids = input_ids[:, -(max_context - max_tokens):]
+                attention_mask = attention_mask[:, -(max_context - max_tokens):]
                 print("updated len", len(input_ids[0]))
                 # max_tokens = max_context - len(input_ids[0])
 
